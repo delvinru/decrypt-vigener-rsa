1da9ab33e7918d89edb54efc772fe5f7a3976df0
delvinru <delvinru@gmail.com>
2020-02-13 22:56:56 +0300

    begin

---
+++ vigener/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(const int argc, char** argv)
+{
+	//use guide
+	if(argc < 2)
+	{
+		printf("./main -f source.txt\n");
+		exit(0);
+	}
+
+	//get file name
+	
+
+	return 0;
+}
